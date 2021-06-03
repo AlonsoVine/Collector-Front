f26fb21d139b754d42c4989f84015aae2e0200d0
Miguel Bautista <bautistaperez3m@gmail.com>
2021-06-03 14:09:49 +0200

    Paginación en Albumes

--- src/app/paginador/paginador-albumes/paginador-albumes.component.ts
+++ src/app/paginador/paginador-albumes/paginador-albumes.component.ts
@@ -30,7 +30,7 @@ export class PaginadorAlbumesComponent implements OnInit {
 
   private initPaginator():void{
     this.desde=Math.min(Math.max(1,this.paginador.number-2),this.paginador.totalPages-5);
-    this.hasta=Math.max(Math.min(this.paginador.totalPages,this.paginador.number+4),7);
+    this.hasta=Math.max(Math.min(this.paginador.totalPages,this.paginador.number+4),6);
     if(this.paginador.totalPages>5){
       this.paginas=new Array(this.hasta-this.desde+1).fill(0).map((_valor,indice)=>indice+this.desde);
     }else{
