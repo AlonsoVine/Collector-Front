7aa6e0a665e15125c59993c63dfacde2ae0d1ffe
Miguel Bautista <bautistaperez3m@gmail.com>
2021-06-03 20:47:00 +0200

    Buscador: Funcionando

--- src/app/album/carta.service.ts
+++ src/app/album/carta.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Carta } from './carta';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
@@ -31,14 +31,19 @@ export class CartaService {
     let url: string = "https://api.scryfall.com/cards";
     return this.http.get(`${url}/${carta.scryfallId}`).pipe(
       map((response: any) => {
-        carta.imagenesCarta = response.image_uris as Map<string, string>
+        if (response.card_faces && response.card_faces[0].image_uris) {
+          carta.imagenesCarta = response.card_faces[0].image_uris as Map<string, string>;
+        } else {
+          carta.imagenesCarta = response.image_uris as Map<string, string>;
+        }
       })
     );
   }
 
-  getByNombreGroupByOracle(nombre: string): Observable<any> {
+  getByNombreGroupByOracle(nombre: string, page: number): Observable<any> {
     let url = this.url + "/mtgdb/carta/nombre";
-    return this.http.get(`${url}/${nombre}/oracle`).pipe(
+    let params = new HttpParams().set("page", page.toString());
+    return this.http.get(`${url}/${nombre}/oracle`, { params }).pipe(
       map((response: any) => {
         (response.content as Carta[]).map(carta => {
           return carta;
@@ -48,9 +53,10 @@ export class CartaService {
     );
   }
 
-  getByNombreGroupByIlust(nombre: string): Observable<any> {
+  getByNombreGroupByIlust(nombre: string, page: number): Observable<any> {
     let url = this.url + "/mtgdb/carta/nombre";
-    return this.http.get(`${url}/${nombre}/ilust`).pipe(
+    let params = new HttpParams().set("page", page.toString());
+    return this.http.get(`${url}/${nombre}/ilust`, { params }).pipe(
       map((response: any) => {
         (response.content as Carta[]).map(carta => {
           return carta;
@@ -60,9 +66,10 @@ export class CartaService {
     );
   }
   
-  getByNombreGroupById(nombre: string): Observable<any> {
+  getByNombreGroupById(nombre: string, page: number): Observable<any> {
     let url = this.url + "/mtgdb/carta/nombre";
-    return this.http.get(`${url}/${nombre}`).pipe(
+    let params = new HttpParams().set("page", page.toString());
+    return this.http.get(`${url}/${nombre}`, { params }).pipe(
       map((response: any) => {
         (response.content as Carta[]).map(carta => {
           return carta;
@@ -72,9 +79,10 @@ export class CartaService {
     );
   }
 
-  getByOracleGroupByIlust(oracle_id: string): Observable<any> {
+  getByOracleGroupByIlust(oracle_id: string, page: number): Observable<any> {
     let url = this.url + "/mtgdb/carta/oracle";
-    return this.http.get(`${url}/${oracle_id}/ilust`).pipe(
+    let params = new HttpParams().set("page", page.toString());
+    return this.http.get(`${url}/${oracle_id}/ilust`, { params }).pipe(
       map((response: any) => {
         (response.content as Carta[]).map(carta => {
           return carta;
@@ -84,9 +92,10 @@ export class CartaService {
     );
   }
 
-  getByIlustGroupById(ilust_id: string): Observable<any> {
+  getByIlustGroupById(ilust_id: string, page: number): Observable<any> {
     let url = this.url + "/mtgdb/carta/ilust";
-    return this.http.get(`${url}/${ilust_id}`).pipe(
+    let params = new HttpParams().set("page", page.toString());
+    return this.http.get(`${url}/${ilust_id}`, { params }).pipe(
       map((response: any) => {
         (response.content as Carta[]).map(carta => {
           return carta;
