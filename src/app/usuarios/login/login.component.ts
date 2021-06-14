8b90bc78aab93ba6613fbc08476693b2481b3e62
Miguel Bautista <bautistaperez3m@gmail.com>
2021-06-14 10:26:05 +0200

    Error login

--- src/app/usuarios/login/login.component.ts
+++ src/app/usuarios/login/login.component.ts
@@ -45,8 +45,8 @@ export class LoginComponent implements OnInit {
         Swal.fire('Login', `Bienvenido ${usuario.username}, has iniciado sesión con éxito`, 'success');
       },
       err => {
-        if (err.status == 400 || err.status == 401) {
-          Swal.fire('Error Login', '¡username o clave incorrectas!', 'error');
+        if (err.status == 400 || err.status == 401 || err.status == 404) {
+          Swal.fire('Error de login', 'Nombre de usuario o clave incorrectos', 'error');
         }
       }
     );
